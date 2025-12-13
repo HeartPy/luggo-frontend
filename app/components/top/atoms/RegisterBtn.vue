06ce625e29ef764706e0a0b396276059a077b5a1
HeartPy <wubenxin9@gmail.com>
2025-12-14 02:03:45 +0900

    refactor(top): トップページアトミックコンポーネントの更新
    - RegisterBtn.vue: アカウント登録リンク先をStripeアカウント登録ページに変更
    - SecTtl.vue: コードフォーマット改善

--- app/components/top/atoms/RegisterBtn.vue
+++ app/components/top/atoms/RegisterBtn.vue
@@ -1,6 +1,6 @@
 <template>
   <NuxtLink
-    to="/account/register"
+    to="/stripe/account"
     class="mx-auto block w-fit cursor-pointer rounded-lg border-2 border-white px-8 py-4 font-semibold text-white transition-all duration-200 hover:bg-white hover:text-gray-800"
   >
     無料でアカウントを作成する
