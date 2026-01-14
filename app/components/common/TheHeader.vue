db704b03047be22399f9c2fd0c1f6b5dc333449b
HeartPy <wubenxin9@gmail.com>
2026-01-14 18:48:14 +0900

    refactor: ロゴ部分のnavタグを削除

--- app/components/common/TheHeader.vue
+++ app/components/common/TheHeader.vue
@@ -2,23 +2,18 @@
   <header class="w-full bg-white">
     <div class="mx-[2vw] flex h-20 items-center justify-between">
       <div>
-        <nav>
-          <NuxtLink
-            class="flex items-center gap-2 p-2.5 hover:opacity-80"
-            to="/"
-          >
-            <figure class="h-6 w-6">
-              <img
-                src="/img/luggo.svg"
-                alt="LugGo"
-                class="h-full w-full object-contain"
-              />
-            </figure>
-            <span class="whitespace-nowrap text-xl font-bold text-gray-800">
-              LugGo
-            </span>
-          </NuxtLink>
-        </nav>
+        <NuxtLink class="flex items-center gap-2 p-2.5 hover:opacity-80" to="/">
+          <figure class="h-6 w-6">
+            <img
+              src="/img/luggo.svg"
+              alt="LugGo"
+              class="h-full w-full object-contain"
+            />
+          </figure>
+          <span class="whitespace-nowrap text-xl font-bold text-gray-800">
+            LugGo
+          </span>
+        </NuxtLink>
       </div>
       <div>
         <nav>
